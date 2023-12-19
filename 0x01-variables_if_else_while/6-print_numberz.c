f7cd58345968cd96597ff59ef3c0353f787dd0cf
SoumayaAlaouiChrifi1 <soumaya.alaoui.chrifi@gmail.com>
2023-12-18 16:16:26 -0800

    print numbers without using char and only putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}
