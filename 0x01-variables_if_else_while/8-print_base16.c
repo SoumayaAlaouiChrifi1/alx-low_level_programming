6f9f17b2274bd70a0d90de9da43d8a0ca04ec6d0
SoumayaAlaouiChrifi1 <soumaya.alaoui.chrifi@gmail.com>
2023-12-18 17:36:29 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * main - print all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char alpha[] = {'A', 'B', 'D', 'E', 'F', '\0'};
+for (i = 0 ; i < 10 ; i++)
+{
+putchar('0' + i);
+}
+for (i = 0 ; alpha[i] != '\0' ; i++)
+{
+putchar(alpha[i]);
+}
+putchar('\n');
+return (0);
+}
