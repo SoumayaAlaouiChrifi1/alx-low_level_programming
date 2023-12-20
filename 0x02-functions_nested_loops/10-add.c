951db2b1b75b8783a422cb6396bad7a319d98527
SoumayaAlaouiChrifi1 <soumaya.alaoui.chrifi@gmail.com>
2023-12-20 09:30:06 -0800

    10-add.c added and main.h updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+* add - calculates the sum of two integers.
+* @a: first integer
+* @b: second integer
+*
+* Return: The sum
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
