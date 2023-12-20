631d8ff3271ba0e0b339f7b04ca02095bae0e1c8
SoumayaAlaouiChrifi1 <soumaya.alaoui.chrifi@gmail.com>
2023-12-20 05:16:27 -0800

    4-isalpha.c added and main.h updated

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include<stdio.h>
+#include<ctype.h>
+/**
+* _isalpha - Write a function that checks foridalpha parameter c
+* @c: character to be checked
+*
+* Return: 1 if c is a letter, lowercase or uppercase , 0 otherwise
+*/
+int _isalpha(int c)
+{
+if (isalpha(c))
+{
+if (islower(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+else
+{
+return (0);
+}
+}
